f0cd53e1a79e06dbfa1899d2c4716b595cd2e7c7
Srishtigoel1 <112386808+Srishtigoel1@users.noreply.github.com>
2024-03-28 12:30:58 +0530

    Add files via upload

---
+++ HOMEWORK/Trees 3/199. Binary Tree Right Side View.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        
+        vector<int>ans;
+          if(root==NULL)
+        {
+            return ans;
+        }
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            vector<int>a;
+            int size=q.size();
+            for(int i=0;i<size;i++)
+            {
+                auto x=q.front();
+                q.pop();
+                a.push_back(x->val);
+                 if(x->left!=NULL)
+                {
+                    q.push(x->left);
+                }
+                if(x->right!=NULL)
+                {
+                    q.push(x->right);
+                }
+               
+            }
+            ans.push_back(a[a.size()-1]);
+
+        }
+        return ans;
+
+        
+    }
+};
